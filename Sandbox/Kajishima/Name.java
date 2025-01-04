27708fe73f784fa1b8e51033ac2bc8482b5f5c1b
ayuka-kajishima <b.mt.as.ny@gmail.com>
2025-01-04 15:05:51 +0900

    EmployeeIdクラスとNameクラスを自分の作業フォルダに作ってみました。

---
+++ Sandbox/Kajishima/Name.java
@@ -0,0 +1,20 @@
+public class Name {
+
+    /**氏名 */
+    private String name;
+
+    /**コンストラクタ */
+    public Name(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 氏名のgetter
+     * @param なし
+     * @return 氏名
+     * @version 1.0.0
+     */
+    public String getName() {
+        return name;
+    }
+}
