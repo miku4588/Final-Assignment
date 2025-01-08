05074e29795394602e36af15738d894138d67c42
miku4588 <miku8283@gmail.com>
2025-01-08 19:42:42 +0900

    Update MainClass.java

--- Sandbox/Okamoto/Provisional Implementation/MainClass.java
+++ Sandbox/Okamoto/Provisional Implementation/MainClass.java
@@ -1,11 +1,3 @@
 public class MainClass {
-    public static void main(String[] args) {
-        Logger logger = new Logger();
-        ErrorHandler errorHandler = new ErrorHandler(logger);
-        EmployeeManager employeeManager = new EmployeeManager();
-
-        // UIを表示
-        ListViewUI listViewUI = new ListViewUI(employeeManager);
-        listViewUI.displayEmployees();
-    }
+  
 }
