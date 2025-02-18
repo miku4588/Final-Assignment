0bd3743c9e340c0051e98d8fb3a2a2c668a43224
ayuka-kajishima <b.mt.as.ny@gmail.com>
2025-02-18 23:55:51 +0900

    loggerクラス作成、それに伴ってmainクラス更新

---
+++ App/src/EmployeeInfoLogger.java
@@ -0,0 +1,24 @@
+/**
+ * ログを取るクラス
+ */
+public class EmployeeInfoLogger {
+
+    /**
+     * ログ保存先のフォルダ
+     */
+    private static final String LOG_FOLDER = "EmployeeInfoApp/Log";
+
+    /**
+     * createLogFolderメソッド
+     */
+    public static void createLogFolder() {
+        // ログフォルダの存在確認　なければ作成、あれば何もしない
+    }
+
+    /**
+     * logOutputメソッド
+     */
+    public static void logOutput(String message) {
+        // ログファイル存在チェック　なければ作成、あればログを残す
+    }
+}
--- App/src/MainApp.java
+++ App/src/MainApp.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.TimeUnit;
+
 /**
  * Mainクラス
  */
@@ -10,8 +12,8 @@ public class MainApp {
     public static void main(String[] args) {
         
         // EmployeeInfoLoggerクラスのメソッドを呼び出す
-        // createLogFolderメソッド…ログフォルダの存在確認　なければ作成、あれば何もしない
-        // logOutputメソッド…ログファイル存在チェック　なければ作成、あれば何もしない
+        EmployeeInfoLogger.createLogFolder(); // ログフォルダの存在確認
+        EmployeeInfoLogger.logOutput("ここにログの内容が入ります（考え中）"); // ログファイル存在チェック
         // 💡ログフォルダのパスは変数ではなく定数に変更（LOG_FOLDER）
 
         EmployeeManager manager = new EmployeeManager(); // EmployeeManager初期化
@@ -19,8 +21,12 @@ public class MainApp {
         // 💡EmployeeManagerクラスの中でデータCSV存在チェック　なければ作成、あればデータを読み込む
         // 💡EmployeeManagerクラスにデータ保存先のパス（DATA_FOLDER）、データCSVのファイル名（CSV_FILE）の定数を用意
 
-        ListViewUI listView = new ListViewUI(); // ListViewUI初期化
-        // 💡ListViewUIの中にはメインメソッド置かずにここで初期化
-        // 💡ListViewUIのコンストラクタの中でdisplayEmployeesメソッドを呼び、画面を表示させる
+        // スレッドの内容　ListViewUI初期化
+        Thread threadListViewUI = new Thread(() -> {
+            ListViewUI listView = new ListViewUI(); // ListViewUI初期化
+        });
+
+        threadListViewUI.start();
+        
     }
 }
