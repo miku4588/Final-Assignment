b0021bd97dd309f104054d721ce9b68679af9fce
ayuka-kajishima <b.mt.as.ny@gmail.com>
2025-03-06 20:00:09 +0900

    Mainクラス・ロガークラス更新　梶嶋

--- App/src/EmployeeInfoLogger.java
+++ App/src/EmployeeInfoLogger.java
@@ -1,3 +1,7 @@
+import java.nio.file.*;
+import java.time.LocalDate;
+import java.util.logging.FileHandler;
+
 /**
  * ログを取るクラス
  */
@@ -6,13 +10,28 @@ public class EmployeeInfoLogger {
     /**
      * ログ保存先のフォルダ
      */
-    private static final String LOG_FOLDER = "EmployeeInfoApp/Log";
+    private static final String LOG_FOLDER = "../EmployeeInfoApp/Log";
 
     /**
      * createLogFolderメソッド
      */
     public static void createLogFolder() {
-        // ログフォルダの存在確認　なければ作成、あれば何もしない
+        try {
+            // ログフォルダを作成
+            Files.createDirectories(Paths.get(LOG_FOLDER));
+            String logFile = LOG_FOLDER + "/EmployeeInfoApp-" + LocalDate.now() + ".log";
+            FileHandler logHandler = new FileHandler(logFile, true);
+
+            // ログのフォーマットを設定
+            // ログの利用方法をよく調べてチームの仕様に合わせて変更・対応してください
+            System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tF %1$tT %4$s %2$s %5$s%6$s%n");
+            logHandler.setFormatter(new SimpleFormatter());
+
+            logger.addHandler(logHandler);
+        } catch (IOException e) {
+            // スタックトレースを文字列で取得してログに出力する
+            printLogStackTrace(e, "ログ設定で例外が発生しました");
+        }
     }
 
     /**
--- App/src/MainApp.java
+++ App/src/MainApp.java
@@ -13,20 +13,27 @@ public class MainApp {
         
         // EmployeeInfoLoggerクラスのメソッドを呼び出す
         EmployeeInfoLogger.createLogFolder(); // ログフォルダの存在確認
-        EmployeeInfoLogger.logOutput("ここにログの内容が入ります（考え中）"); // ログファイル存在チェック
+        EmployeeInfoLogger.logOutput("アプリを起動しました。"); // ログファイル存在確認
         // 💡ログフォルダのパスは変数ではなく定数に変更（LOG_FOLDER）
 
         EmployeeManager manager = new EmployeeManager(); // EmployeeManager初期化
-        // 💡データ読み込みもメソッド化した方がいいかも…EmployeeManagerクラスにメソッド追加しましょうか！！
-        // 💡EmployeeManagerクラスの中でデータCSV存在チェック　なければ作成、あればデータを読み込む
-        // 💡EmployeeManagerクラスにデータ保存先のパス（DATA_FOLDER）、データCSVのファイル名（CSV_FILE）の定数を用意
 
-        // スレッドの内容　ListViewUI初期化
+        // サブスレッド内でデータ読み込み
         Thread threadListViewUI = new Thread(() -> {
-            ListViewUI listView = new ListViewUI(); // ListViewUI初期化
+
+            // 💡EmployeeManagerクラスの中でデータCSV存在チェック なければ作成、あればデータを読み込む
+            // 💡EmployeeManagerクラスにデータ保存先のパス（DATA_FOLDER）、データCSVのファイル名（CSV_FILE）の定数を用意
+
         });
 
         threadListViewUI.start();
-        
+
+        try {
+            threadListViewUI.join(); // サブスレッドの終了を待機
+        } catch (InterruptedException e) {
+            e.printStackTrace();  // サブスレッドに割り込みが入るとInterruptedExceptionエラーを吐くのでキャッチ
+        }
+
+        ListViewUI listView = new ListViewUI(); // ListViewUI初期化
     }
 }
