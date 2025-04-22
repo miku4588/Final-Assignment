ef0840171ee8ad59961823f8d1d8790a3b29068c
ayuka-kajishima <b.mt.as.ny@gmail.com>
2025-04-22 18:48:50 +0900

    EmployeeInfoにゲッターメソッドを追加　梶嶋

--- App/src/EmployeeInfo.java
+++ App/src/EmployeeInfo.java
@@ -1,3 +1,7 @@
+import java.time.LocalDate;
+import java.time.Year;
+import java.time.YearMonth;
+
 /**
  * 社員情報の各項目をまとめるクラス
  */
@@ -65,4 +69,110 @@ class EmployeeInfo {
                remarks.getRemarks(); // + "," +
             //    languages.getLanguages();
     }
+
+    /**
+     * EmployeeInfoからEmployeeIdを取得
+     * @return 社員ID
+     */
+    public String getEmployeeId() {
+        return employeeId.getEmployeeId();
+    }
+    
+    /**
+     * EmployeeInfoからNameを取得
+     * @return 氏名
+     */
+    public String getName() {
+        return name.getName();
+    }
+
+    /**
+     * EmployeeInfoからPhoneticを取得
+     * @return 氏名カナ
+     */
+    public String getPhonetic() {
+        return phonetic.getPhonetic();
+    }
+
+    /**
+     * EmployeeInfoからBirthDateを取得
+     * @return 誕生日
+     */
+    public LocalDate getBirthDate() {
+        return birthDate.getBirthDate();
+    }
+
+    /**
+     * EmployeeInfoからJoinYearMonthを取得
+     * @return 入社年月
+     */
+    public YearMonth getJoinYearMonth() {
+        return joinYearMonth.getJoinYearMonth();
+    }
+
+    /**
+     * EmployeeInfoからEngineerStartYearを取得
+     * @return エンジニア開始年
+     */
+    public Year getEngineerStartYear() {
+        return engineerStartYear.getEngineerStartYear();
+    }
+
+    /**
+     * EmployeeInfoからTechnicalSkillを取得
+     * @return 技術スキル
+     */
+    public double getTechnicalSkill() {
+        return technicalSkill.getTechnicalSkill();
+    }
+
+    /**
+     * EmployeeInfoからAttitudeを取得
+     * @return 態度
+     */
+    public double getAttitude() {
+        return attitude.getAttitude();
+    }
+
+    /**
+     * EmployeeInfoからCommunicationSkillを取得
+     * @return コミュニケーション能力
+     */
+    public double getCommunicationSkill() {
+        return communicationSkill.getCommunicationSkill();
+    }
+
+    /**
+     * EmployeeInfoからLeadershipを取得
+     * @return リーダーシップ
+     */
+    public double getLeadership() {
+        return leadership.getLeadership();
+    }
+
+    /**
+     * EmployeeInfoからCareerを取得
+     * @return 経歴
+     */
+    public String getCareer() {
+        return career.getCareer();
+    }
+
+    /**
+     * EmployeeInfoからTrainingHistoryを取得
+     * @return 研修履歴
+     */
+    public String getTrainingHistory() {
+        return trainingHistory.getTrainingHistory();
+    }
+
+    /**
+     * EmployeeInfoからRemarksを取得
+     * @return 備考
+     */
+    public String getRemarks() {
+        return remarks.getRemarks();
+    }
+
+    
 }
