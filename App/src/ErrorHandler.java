48504e572252a102d00e5530d64d774de385de07
ayuka-kajishima <b.mt.as.ny@gmail.com>
2025-05-07 00:20:17 +0900

    エラーメッセージ表示処理をinvokeLaterで包みました。　梶嶋

--- App/src/ErrorHandler.java
+++ App/src/ErrorHandler.java
@@ -1,8 +1,8 @@
 import java.awt.Dimension;
-
 import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
 
 /**
  * エラー処理をするクラス
@@ -14,19 +14,23 @@ public class ErrorHandler {
      */
     public static void handleError(String errorMessage){
 
-        // マウスでコピー可能なテキストを設定
-        JTextArea textArea = new JTextArea(errorMessage);
-        textArea.setEditable(false); // 編集を不可にする
-        textArea.setLineWrap(true);  // 自動改行
-        textArea.setWrapStyleWord(true); // 単語単位で改行
-        textArea.setCaretPosition(0); // 初期位置を先頭に設定
+        // 画面に関する処理はinvokeLaterで囲むのが安全
+        SwingUtilities.invokeLater(() -> {
 
-        // スクロール可能なペインを設定
-        JScrollPane scrollPane = new JScrollPane(textArea);
-        scrollPane.setPreferredSize(new Dimension(400, 200));
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-
-        // ダイアログを表示
-        JOptionPane.showMessageDialog(null, scrollPane, "エラー", JOptionPane.ERROR_MESSAGE);
+            // マウスでコピー可能なテキストを設定
+            JTextArea textArea = new JTextArea(errorMessage);
+            textArea.setEditable(false); // 編集を不可にする
+            textArea.setLineWrap(true);  // 自動改行
+            textArea.setWrapStyleWord(true); // 単語単位で改行
+            textArea.setCaretPosition(0); // 初期位置を先頭に設定
+    
+            // スクロール可能なペインを設定
+            JScrollPane scrollPane = new JScrollPane(textArea);
+            scrollPane.setPreferredSize(new Dimension(400, 200));
+            scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+    
+            // ダイアログを表示
+            JOptionPane.showMessageDialog(null, scrollPane, "エラー", JOptionPane.ERROR_MESSAGE);
+        });
     }
 }
