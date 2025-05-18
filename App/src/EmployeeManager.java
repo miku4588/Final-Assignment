8efa48a6976e315549141c5e044c2f1e19e6b32f
ayuka-kajishima <b.mt.as.ny@gmail.com>
2025-05-18 14:01:03 +0900

    EmployeeManagerをシングルトンパターンに変更　梶嶋

--- App/src/EmployeeManager.java
+++ App/src/EmployeeManager.java
@@ -8,14 +8,18 @@ public class EmployeeManager {
 
     // EmployeeInfoのList
     private List<EmployeeInfo> employeeList = new ArrayList<>();
+    // EmployeeManagerのインスタンス
+    private static EmployeeManager instance;
+
 
     /**
      * コンストラクタ
      */
-    public EmployeeManager(List<EmployeeInfo> employeeList) {
+    private EmployeeManager(List<EmployeeInfo> employeeList) {
         this.employeeList = employeeList;
     }
 
+    
     /**
      * ゲッター
      * @return employeeList
@@ -24,11 +28,27 @@ public class EmployeeManager {
         return employeeList;
     }
 
+
     /**
-     * セッター
+     * インスタンスを呼び出す（なければ生成）
+     * @param employeeList
+     * @return EmployeeManagerのインスタンス
+     */
+    public static EmployeeManager getInstance() {
+        if (instance == null) {
+            throw new IllegalStateException("EmployeeManagerはまだ初期化されていません。");
+        }
+        return instance;
+    }
+
+
+    /**
+     * EmployeeManagerの初期化
      * @param employeeList
      */
-    public void setEmployeeList(List<EmployeeInfo> employeeList) {
-        this.employeeList = employeeList;
+    public static void initializeEmployeeManager(List<EmployeeInfo> employeeList) {
+        if (instance == null) {
+            instance = new EmployeeManager(employeeList);
+        }
     }
 }
