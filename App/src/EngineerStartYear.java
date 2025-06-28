014dc6b2c7691135700bda864e51c000545b6ad8
miku4588 <miku8283@gmail.com>
2025-06-28 23:03:09 +0900

    Update EngineerStartYear.java

--- App/src/EngineerStartYear.java
+++ App/src/EngineerStartYear.java
@@ -7,17 +7,11 @@ import java.time.Year;
 class EngineerStartYear extends EmployeeInfoValidator {
     private Year engineerStartYear;
 
-    public EngineerStartYear(String engineerStartYear) {
-        this.engineerStartYear = Year.parse(engineerStartYear);
-        validate();  // ここで入力チェックを実施
+   public EngineerStartYear(String year) {
+        this.engineerStartYear = Year.parse(year);
     }
 
-    @Override
-    protected void validate() {
-        if (!validateInput(engineerStartYear.toString())) {
-            throw new IllegalArgumentException("エンジニアとしての開始年は過去〜現在の範囲の西暦(YYYY)で入力してください。");
-        }
-    }
+
 
     @Override
     protected boolean validateInput(String engineerStartYear) {
