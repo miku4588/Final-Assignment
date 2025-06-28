6b204670cdc5b6cce92ff22cb652f10ff2cea2f8
miku4588 <miku8283@gmail.com>
2025-06-28 22:56:37 +0900

    Update Career.java

--- App/src/Career.java
+++ App/src/Career.java
@@ -1,27 +1,13 @@
 class Career extends EmployeeInfoValidator {
     private String career;
-    private String input;
 
     public Career(String career) {
-        this.input = career;
-    }
-
-    @Override
-    protected void validate() {
-        if (!validateInput(input)) {
-            throw new IllegalArgumentException("経歴情報は1〜500文字以内で入力してください。");
-        }
-        this.career = input;
+        this.career = career;
     }
 
     @Override
     protected boolean validateInput(String career) {
-        return career != null && validateLength(career, 1, 500);
-    }
-
-    @Override
-    protected boolean validateLength(String value, int min, int max) {
-        return value != null && value.length() >= min && value.length() <= max;
+        return career != null && validateLength(career, 0, 500);
     }
 
     public String getCareer() {
